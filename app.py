d6d297974468ee0e322d8f469b5cdcf1283090c5
louiedweck <dweck.louis@gmail.com>
2019-10-17 16:54:03 -0700

    still first commit

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from random import randint
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     """Return homepage."""
     return 'Hello, world!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
